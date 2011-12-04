ca0a29b259824acd5bf2baade1445c7b88e0f5c8
Jan-Philippe Lavoie <janphilippelavoie@gmail.com>
2011-12-03 19:13:24 -0500

    password encryption and authenticate method done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -32,14 +21,49 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
+  # Compares encrypted_password with the encrypted version of submitted_password
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+
   private
   
   def encrypt_password
+    self.salt = make_salt if new_record?
     self.encrypted_password = encrypt(password)
   end
 
   def encrypt(string)
-    string
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
   end
   
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
